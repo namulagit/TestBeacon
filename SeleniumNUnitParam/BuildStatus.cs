20396b12d46acf80ffb55ecdf94e339536d4c75b
Syed Daud <syed.daud@india-synverse.com>
2017-12-14 21:19:27 +0530

    sorce test

---
+++ SeleniumNUnitParam/BuildStatus.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNUnitParam
+{
+    public class BuildStatus : Hooks
+    {
+        [Test]
+        public void LoginCheck()
+        {  
+            TimeSpan pageloadwait = TimeSpan.FromSeconds(30.00);
+            string date = DateTime.Now.AddDays(-1).ToString("yyyyMMdd", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+            Driver.Navigate().GoToUrl("http://Synverse%2ETest3:st%3A9872%40s@sorce-tfsbunker:8080/tfs/DefaultCollection/Beacon/Beacon%20Team/_build#definitionUri=&_a=completed");
+            System.Threading.Thread.Sleep(10000);
+            Driver.Manage().Timeouts().ImplicitlyWait(pageloadwait);
+            string BuildStatus = Driver.FindElement(By.XPath("(//*[contains(text(),'Engage Nightly_" + date + "')])/preceding-sibling::div[1]")).GetAttribute("title");
+            Assert.AreEqual("Succeeded", BuildStatus, "BuildStatus");
+            Driver.Close();
+        }
+
+    }
+}
---
+++ SeleniumNUnitParam/EngageLogin.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNUnitParam
+{
+    public class EngageLogin : Hooks
+    {
+
+
+        [Test]
+        public void LoginCheck()
+        {
+            Driver.Navigate().GoToUrl("http://loaduser1:Vanadium2200@beacon-test/sorce/");
+            System.Threading.Thread.Sleep(10000);
+            TimeSpan pageloadwait = TimeSpan.FromSeconds(30.00);
+            Driver.Manage().Timeouts().ImplicitlyWait(pageloadwait);
+            string title = Driver.Title;
+            Assert.AreEqual("Homepage", title, "Page Title");
+            Driver.Close();
+        }
+        [Test]
+        public void LoginCheck2()
+        {
+            Driver.Navigate().GoToUrl("http://Sorce%5CSynverse%2ETest1:st%3A9874%40s@beacon-test/sorce/beacon/");
+            System.Threading.Thread.Sleep(5000);
+            TimeSpan pageloadwait = TimeSpan.FromSeconds(30.00);
+            Driver.Manage().Timeouts().ImplicitlyWait(pageloadwait);
+            string title = Driver.Title;
+            Assert.AreEqual("Homepage", title, "Page Title");
+            System.Threading.Thread.Sleep(3000);
+            Driver.Close();
+        }
+
+
+    }
+}
