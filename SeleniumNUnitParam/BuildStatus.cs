2bfb9f0825bbcaa2470cfe9182e9a86570004d4b
namulagit <naveengoud418@gmail.com>
2018-01-04 22:55:28 +0530

    Changing code from navigate to -URL

--- SeleniumNUnitParam/BuildStatus.cs
+++ SeleniumNUnitParam/BuildStatus.cs
@@ -15,7 +15,7 @@ namespace SeleniumNUnitParam
         {  
             TimeSpan pageloadwait = TimeSpan.FromSeconds(30.00);
             string date = DateTime.Now.AddDays(-1).ToString("yyyyMMdd", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
-            Driver.Navigate().GoToUrl("http://Synverse%2ETest3:st%3A9872%40s@sorce-tfsbunker:8080/tfs/DefaultCollection/Beacon/Beacon%20Team/_build#definitionUri=&_a=completed");
+            Driver.Url="http://Synverse%2ETest3:st%3A9872%40s@sorce-tfsbunker:8080/tfs/DefaultCollection/Beacon/Beacon%20Team/_build#definitionUri=&_a=completed";
             System.Threading.Thread.Sleep(10000);
             Driver.Manage().Timeouts().ImplicitlyWait(pageloadwait);
             string BuildStatus = Driver.FindElement(By.XPath("(//*[contains(text(),'Engage Nightly_" + date + "')])/preceding-sibling::div[1]")).GetAttribute("title");
